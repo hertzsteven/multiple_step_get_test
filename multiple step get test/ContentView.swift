bee53ceea8ff32863db2c1cc90bd4a8785860ce4
Steven Hertz <StevenH@appi4.com>
2024-02-13 08:10:07 -0500

    Initial Commit

---
+++ multiple step get test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  multiple step get test
+//
+//  Created by Steven Hertz on 2/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ multiple step get test/multiple_step_get_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  multiple_step_get_testApp.swift
+//  multiple step get test
+//
+//  Created by Steven Hertz on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct multiple_step_get_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
